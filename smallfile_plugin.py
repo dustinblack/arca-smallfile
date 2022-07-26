dc9b02fc165998c04c44ca24c449c77375f99139
Dustin Black <dblack@redhat.com>
2022-07-26 22:27:25 +0200

    add tempfile cleanup on exit
    todos and comments
    
    initial work for getting output as json format
    
    add cleanup example; make file_size optional; improve error output
    
    enable smallfile cleanup operation to run as a boolean choice

--- smallfile_plugin.py
+++ smallfile_plugin.py
@@ -22,6 +22,9 @@ import tempfile
 import yaml
 import subprocess
 import dataclasses
+import fileinput
+import os
+import shutil
 from dataclasses import dataclass
 from typing import List
 from wolkenwalze_plugin_sdk import plugin
@@ -35,9 +38,11 @@ class SmallfileParams:
     """
     top: str
     operation: str
-    threads: int
-    files: int
-    file_size: str = dataclasses.field(metadata={"id": "file-size"})
+    file_size: typing.Optional[str] = dataclasses.field(default=None, metadata={"id": "file-size"})
+    #FIXME: wolkenwalze throws an error when using typing.Optional with an int and the key is empty
+    threads: typing.Optional[int] = None
+    files: typing.Optional[int] = None
+    #TODO: Expand supported parameters and determine defaults/requirements
 
 
 @dataclass
@@ -45,7 +50,10 @@ class WorkloadParams:
     """
     This is the data structure for the input parameters of the step defined below.
     """
+    #FIXME: Enable boolean types
+    cleanup: str
     samples: int
+    #TODO: Implement samples? Or do we do this outside of the plugin?
     SmallfileParams: SmallfileParams
     SmallfileRelease: typing.Optional[str] = None
 
@@ -56,7 +64,7 @@ class WorkloadResults:
     This is the output data structure for the success case.
     """
     #TODO
-    stdout: str
+    json: str
 
 
 @dataclass
@@ -70,13 +78,13 @@ class WorkloadError:
 
 smallfile_schema = plugin.build_object_schema(SmallfileParams)
 
-# The following is a decorator (starting with @). We add this in front of our function to define the metadata for our
-# step.
+# The following is a decorator (starting with @). We add this in front of our function to define the metadata for our step.
 @plugin.step(
     id="workload",
     name="smallfile workload",
     description="Run the smallfile workload with the given parameters",
     outputs={"success": WorkloadResults, "error": WorkloadError},
+    #TODO: Add step for cache dropping; Or do we handle this in a separate plugin?
 )
 
 def smallfile_run(params: WorkloadParams) -> typing.Tuple[str, typing.Union[WorkloadResults, WorkloadError]]:
@@ -90,15 +98,17 @@ def smallfile_run(params: WorkloadParams) -> typing.Tuple[str, typing.Union[Work
     """
 
     smallfile_url = "https://github.com/distributed-system-analysis/smallfile.git"
-    smallfile_release = WorkloadParams.SmallfileRelease or "1.1"
+    smallfile_release = params.SmallfileRelease or "1.1"
     smallfile_dir = tempfile.mkdtemp()
     smallfile_yaml = tempfile.mkstemp()
-    #TODO: Clean up tempdir/file
+    smallfile_out = tempfile.mkstemp()
 
+    # Clone the smallfile repo and checkout the correct release
     Repo.clone_from(smallfile_url, smallfile_dir)
     g = Git(smallfile_dir)
     g.checkout(smallfile_release)
 
+    # Copy all parameters from SmallfileParams directly for the smallfile CLI to use via YAML
     with open(smallfile_yaml[1], 'w') as file:
         file.write(yaml.dump(smallfile_schema.serialize(params.SmallfileParams)))
 
@@ -106,21 +116,57 @@ def smallfile_run(params: WorkloadParams) -> typing.Tuple[str, typing.Union[Work
         "{}/smallfile_cli.py".format(smallfile_dir),
         "--yaml-input-file",
         smallfile_yaml[1],
+        "--output-json",
+        smallfile_out[1],
+        "--response-times",
+        "y",
     ]
 
+    # Run smallfile
     try:
-        proc = subprocess.check_output(smallfile_cmd, text=True, cwd=smallfile_dir, stderr=subprocess.STDOUT)
+        print(subprocess.check_output(smallfile_cmd, cwd=smallfile_dir, text=True, stderr=subprocess.STDOUT))
     except subprocess.CalledProcessError as error:
-        return "error", WorkloadError("smallfile_cli.py failed with return code {}".format(error.returncode))
+        temp_cleanup(smallfile_yaml, smallfile_dir)
+        return "error", WorkloadError("{} failed with return code {}:\n{}".format(error.cmd[0],error.returncode,error.output))
 
-    return "success", WorkloadResults(proc)
+    # Cleanup after run, if enabled
+    if params.cleanup:
+        print("Test complete; Cleaning up operation files...")
+        cleanup_yaml = smallfile_schema.serialize(params.SmallfileParams)
+        cleanup_yaml["operation"] = "cleanup"
+        smallfile_cleanup_yaml = tempfile.mkstemp()
+        smallfile_cleanup_cmd = [
+            "{}/smallfile_cli.py".format(smallfile_dir),
+            "--yaml-input-file",
+            smallfile_cleanup_yaml[1],
+        ]
+        with open(smallfile_cleanup_yaml[1], 'w') as file:
+            file.write(yaml.dump(cleanup_yaml))
+        try:
+            print(subprocess.check_output(smallfile_cleanup_cmd, cwd=smallfile_dir, text=True, stderr=subprocess.STDOUT))
+        except subprocess.CalledProcessError as error:
+            temp_cleanup(smallfile_yaml, smallfile_dir)
+            temp_cleanup(smallfile_cleanup_yaml)
+            return "error", WorkloadError("{} failed with return code {}:\n{}".format(error.cmd[0],error.returncode,error.output))
+        temp_cleanup(smallfile_cleanup_yaml)
 
 
-    #return "error", WorkloadError(
-    #    "Cannot kill pod %s in namespace %s, function not implemented" % (
-    #        params.pod_name_pattern.pattern,
-    #        params.namespace_pattern.pattern
-    #    ))
+    with open(smallfile_out[1], 'r') as output:
+        smallfile_results = output.read()
+    temp_cleanup(smallfile_yaml, smallfile_dir)
+    return "success", WorkloadResults(smallfile_results)
+
+
+
+def temp_cleanup(file = [], directory = ""):
+    # Cleanup our temp files
+    print("Cleaning up temporary files...")
+    if file:
+        os.close(file[0])
+        os.remove(file[1])
+    if directory:
+        shutil.rmtree(directory)
+
 
 
 if __name__ == "__main__":

219e8e793c544c5c0434c79f5b0a8d5966d26e29
Dustin Black <dblack@redhat.com>
2022-07-27 11:40:29 +0200

    initial framework for response time post-processing
    post processing to collect response times
    
    better runtime status to stdout
    
    collect response times in output as string for now
    
    minor

--- smallfile_plugin.py
+++ smallfile_plugin.py
@@ -20,6 +20,7 @@ import sys
 import typing
 import tempfile
 import yaml
+import json
 import subprocess
 import dataclasses
 import fileinput
@@ -39,7 +40,6 @@ class SmallfileParams:
     top: str
     operation: str
     file_size: typing.Optional[str] = dataclasses.field(default=None, metadata={"id": "file-size"})
-    #FIXME: arcaflow throws an error when using typing.Optional with an int and the key is empty
     threads: typing.Optional[int] = None
     files: typing.Optional[int] = None
     #TODO: Expand supported parameters and determine defaults/requirements
@@ -64,7 +64,15 @@ class WorkloadResults:
     This is the output data structure for the success case.
     """
     #TODO
-    json: str
+    results: str
+    rsptimes: str
+    #WIP
+    #timestamp:
+    #params:
+        #param:
+    #results:
+        #result:
+        #how do we handle the variable nature of threads?
 
 
 @dataclass
@@ -100,38 +108,82 @@ def smallfile_run(params: WorkloadParams) -> typing.Tuple[str, typing.Union[Work
     smallfile_url = "https://github.com/distributed-system-analysis/smallfile.git"
     smallfile_release = params.SmallfileRelease or "1.1"
     smallfile_dir = tempfile.mkdtemp()
-    smallfile_yaml = tempfile.mkstemp()
-    smallfile_out = tempfile.mkstemp()
+    smallfile_yaml_file = tempfile.mkstemp()
+    smallfile_out_file = tempfile.mkstemp()
 
     # Clone the smallfile repo and checkout the correct release
+    print("==>> Getting smallfile release {} ...".format(smallfile_release))
     Repo.clone_from(smallfile_url, smallfile_dir)
     g = Git(smallfile_dir)
     g.checkout(smallfile_release)
 
     # Copy all parameters from SmallfileParams directly for the smallfile CLI to use via YAML
-    with open(smallfile_yaml[1], 'w') as file:
-        file.write(yaml.dump(smallfile_schema.serialize(params.SmallfileParams)))
+    print("==>> Importing workload parameters ...")
+    smallfile_params= smallfile_schema.serialize(params.SmallfileParams)
+    with open(smallfile_yaml_file[1], 'w') as file:
+        file.write(yaml.dump(smallfile_params))
 
     smallfile_cmd = [
         "{}/smallfile_cli.py".format(smallfile_dir),
         "--yaml-input-file",
-        smallfile_yaml[1],
+        smallfile_yaml_file[1],
         "--output-json",
-        smallfile_out[1],
+        smallfile_out_file[1],
         "--response-times",
         "y",
     ]
 
-    # Run smallfile
+    # Run smallfile workload
+    print("==>> Running smallfile workload ...")
     try:
         print(subprocess.check_output(smallfile_cmd, cwd=smallfile_dir, text=True, stderr=subprocess.STDOUT))
     except subprocess.CalledProcessError as error:
-        temp_cleanup(smallfile_yaml, smallfile_dir)
+        temp_cleanup(smallfile_yaml_file, smallfile_dir)
         return "error", WorkloadError("{} failed with return code {}:\n{}".format(error.cmd[0],error.returncode,error.output))
+
+    with open(smallfile_out_file[1], 'r') as output:
+        smallfile_results = output.read()
+
+    smallfile_json = json.loads(smallfile_results)
+    #debug
+    #print(smallfile_results)
+    #print(smallfile_json)
+
+    #TODO: Convert results into schema data
+
+
+    # Post-process output for response times
+    print("==>> Collecting response times ...")
+    elapsed_time = float(smallfile_json["results"]["elapsed"])
+    start_time = smallfile_json["results"]["startTime"]
+    rsptime_dir = os.path.join(smallfile_params["top"], "network_shared")
+    smallfile_process_cmd = [
+        "{}/smallfile_rsptimes_stats.py".format(smallfile_dir),
+        "--time-interval",
+        str(max(int(elapsed_time / 120.0), 1)),
+        "--start-time",
+        str(int(start_time)),
+        rsptime_dir,
+    ]
+
+    try:
+        print(subprocess.check_output(smallfile_process_cmd, cwd=smallfile_dir, text=True, stderr=subprocess.STDOUT))
+    except subprocess.CalledProcessError as error:
+        temp_cleanup(smallfile_out_file, smallfile_dir)
+        return "error", WorkloadError("{} failed with return code {}:\n{}".format(error.cmd[0],error.returncode,error.output))
+
+    with open("{}/stats-rsptimes.csv".format(rsptime_dir), 'r') as output:
+        smallfile_rsptimes = output.read()
+
+    #debug
+    #print(smallfile_rsptimes)
+    
+    #TODO: Convert response times into schema data
+
 
     # Cleanup after run, if enabled
     if params.cleanup:
-        print("Test complete; Cleaning up operation files...")
+        print("==>> Cleaning up operation files ...")
         cleanup_yaml = smallfile_schema.serialize(params.SmallfileParams)
         cleanup_yaml["operation"] = "cleanup"
         smallfile_cleanup_yaml = tempfile.mkstemp()
@@ -145,22 +197,22 @@ def smallfile_run(params: WorkloadParams) -> typing.Tuple[str, typing.Union[Work
         try:
             print(subprocess.check_output(smallfile_cleanup_cmd, cwd=smallfile_dir, text=True, stderr=subprocess.STDOUT))
         except subprocess.CalledProcessError as error:
-            temp_cleanup(smallfile_yaml, smallfile_dir)
+            temp_cleanup(smallfile_yaml_file, smallfile_dir)
             temp_cleanup(smallfile_cleanup_yaml)
             return "error", WorkloadError("{} failed with return code {}:\n{}".format(error.cmd[0],error.returncode,error.output))
         temp_cleanup(smallfile_cleanup_yaml)
 
+    temp_cleanup(smallfile_yaml_file, smallfile_dir)
+    temp_cleanup(smallfile_out_file)
 
-    with open(smallfile_out[1], 'r') as output:
-        smallfile_results = output.read()
-    temp_cleanup(smallfile_yaml, smallfile_dir)
-    return "success", WorkloadResults(smallfile_results)
+    print("==>> Workload run complete!")
+    return "success", WorkloadResults(smallfile_results,smallfile_rsptimes)
 
 
 
 def temp_cleanup(file = [], directory = ""):
     # Cleanup our temp files
-    print("Cleaning up temporary files...")
+    print("==>> Cleaning up temporary files ...")
     if file:
         os.close(file[0])
         os.remove(file[1])
